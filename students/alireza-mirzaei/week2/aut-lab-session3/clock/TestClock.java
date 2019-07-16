dbf49169978dd9b3d8d5fa1a0b33ac8b41fa3565
alireza <alirezaxmz@gmail.com>
2019-07-16 05:01:16 +0430

    ClockDisplay.java

---
+++ students/alireza-mirzaei/week2/aut-lab-session3/clock/TestClock.java
@@ -0,0 +1,13 @@
+
+public class TestClock {
+
+    public static void main(String[] args) {
+
+
+        ClockDisplay clock = new ClockDisplay();
+        clock.setTime(0,0,0);
+        System.out.println(clock.getTime());
+
+
+    }
+}
